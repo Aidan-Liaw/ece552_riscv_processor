bb2177092bae0a0d317d020c7f763f4a67c263dd
askjsd <dhan77889@gmail.com>
2026-02-21 15:30:20 -0600

    untested controlunit

--- rtl/control_unit.v
+++ rtl/control_unit.v
@@ -2,101 +2,159 @@
 
 module control_unit (
   input wire [6:0] opcode,
+  input wire [2:0] func3,
 
   // mux controls
   output reg RegSignPropagation,
-  output reg RegReadSize,
-  output reg immFormat [5:0],
+  output reg [1:0] RegReadSize,
+  output reg [5:0] immFormat,
   output reg pcAdd,
-  output reg memMask[3:0],
-  output reg registerWriteSel,
+  output reg [3:0] memMask,
+  output reg [1:0] registerWriteSel,
   output reg aluSrc,      // 0 = reg, 1 = imm
-  output reg emtoReg,   // 0 = alu result, 1 = mem data
-  output reg regWrite,    // 1 = write too rd
+  output reg regWrite,    // 1 = write to rd
   output reg memRead,     // 1 = read from mem
   output reg memWrite,    // 1 = write to mem
   output reg branch,       // 1 = branch instruction 
   output reg jump,         // 1 = jal or jalr
   output reg halt,         // 1 = EBREAK, stop processor
   output reg retire,
-  output reg [1:0] aluOP[3:0]
+  output reg [1:0] aluOP
 );
 
   always @(*) begin
-    alu_src = 1'b0;
-    mem_to_reg = 1'b0;
-    reg_write = 1'b0;
-    mem_read = 1'b0;
-    mem_write = 1'b0;
+    RegSignPropagation = 1'b0;
+    //default size is a word
+    RegReadSize = 2'b10;
+    immFormat = 6'b0;
+    pcAdd = 1'b0;
+    memMask = 4'b0000;
+    //alu = 00, dmem = 01, imm = 10, pc = 11
+    registerWriteSel = 2'b00;
+    aluSrc = 1'b0;
+    regWrite = 1'b0;
+    memRead = 1'b0;
+    memWrite = 1'b0;
     branch = 1'b0;
     jump = 1'b0;
-    stop = 1'b0;
-    alu_op_type = 2'b00;
+    halt = 1'b0;
+    retire = 1'b1;
+    aluOP = 2'b00;
 
     case (opcode)
       // r-type arithmetic
       7'b0110011: begin
-        reg_write = 1'b1;
-        alu_op_type = 2'b10;
+        regWrite = 1'b1;
+        aluSrc = 1'b0;
+        aluOP = 2'b10;
+        immFormat = 6'b000001; 
+        registerWriteSel = 2'b00; 
       end
 
       // i-type arithmetic
       7'b0010011: begin
-        alu_src = 1'b1;
-        reg_write = 1'b1;
-        alu_op_type = 2'b11;
+        regWrite = 1'b1;
+        aluSrc = 1'b1;
+        aluOP = 2'b11;
+        immFormat = 6'b000010;
+        registerWriteSel = 2'b00;
       end
 
       // load instruction 
       7'b0000011: begin
-        alu_src = 1'b1;
-        mem_to_reg = 1'b1;
-        reg_write = 1'b1;
-        mem_read = 1'b1;
-        alu_op_type = 2'b00;
+        regWrite = 1'b1;
+        aluSrc = 1'b1;
+        memRead = 1'b1;
+        aluOP = 2'b00;
+        immFormat = 6'b000010;
+        registerWriteSel = 2'b01;
+        case (func3)
+            //byte
+            3'b000: begin
+                RegReadSize = 2'b00; 
+                RegSignPropagation = 1'b1;
+            end
+            //half
+            3'b001: begin
+                RegReadSize = 2'b01; 
+                RegSignPropagation = 1'b1;
+            end
+            //word
+            3'b010: begin
+                RegReadSize = 2'b10; 
+                RegSignPropagation = 1'b1;
+            end
+            //byte unsigned
+            3'b100: begin
+                RegReadSize = 2'b00; 
+                RegSignPropagation = 1'b0;
+            end
+            //half unsigned
+            3'b101: begin
+                RegReadSize = 2'b01; 
+                RegSignPropagation = 1'b0;
+            end
+        endcase
       end
 
       // store instruction
       7'b0100011: begin
-        alu_src = 1'b1;
-        mem_write = 1'b1;
-        alu_op_type = 2'b00;
+        aluSrc = 1'b1;
+        memWrite = 1'b1;
+        aluOP = 2'b00;
+        immFormat = 6'b000100;
+        case (func3)
+            3'b000: memMask = 4'b0001;
+            3'b001: memMask = 4'b0011;
+            3'b010: memMask = 4'b1111;
+        endcase
       end
 
       // branch
       7'b1100011: begin
         branch = 1'b1;
-        alu_op_type = 2'b01;
+        aluOP = 2'b01;
+        immFormat = 6'b001000;
       end
 
       // jal
       7'b1101111: begin
         jump = 1'b1;
-        reg_write = 1'b1;
+        regWrite = 1'b1;
+        immFormat = 6'b100000;
+        registerWriteSel = 2'b11; 
       end
 
       // jalr
       7'b1100111: begin
         jump = 1'b1;
-        reg_write 1'b1;
-        alu_src = 1'b1;
-        alu_op_type 2'b00;
+        regWrite = 1'b1;
+        aluSrc = 1'b1;
+        aluOP = 2'b00;
+        immFormat = 6'b000010;
+        registerWriteSel = 2'b11;
       end
 
       // lui
       7'b0110111: begin
-        reg_write = 1'b1;
-        alu_src = 1'b1;
+        regWrite = 1'b1;
+        aluSrc = 1'b1;
+        immFormat = 6'b010000;
+        registerWriteSel = 2'b10;
       end
 
       // auipc
-      7'b0010111:
-        reg_write = 1'b1;
+      7'b0010111: begin
+        regWrite = 1'b1;
+        aluSrc = 1'b1;
+        pcAdd = 1'b1;
+        immFormat = 6'b010000; 
+        registerWriteSel = 2'b00; 
       end
 
       // EBREAK
       7'b1110011: begin
-        stop = 1'b1;
+        halt = 1'b1;
       end
     endcase
   end
