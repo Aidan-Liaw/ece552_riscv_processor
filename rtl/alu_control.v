e59a91a56ba7c9cebced3255d74b5182bff063b3
askjsd <dhan77889@gmail.com>
2026-02-22 17:58:36 -0600

    impelmentalu_control

--- rtl/alu_control.v
+++ rtl/alu_control.v
@@ -1,38 +1,32 @@
 `default_nettype wire
 
 module alu_control (
-  input wire [1:0] alu_op_type,
-  input wire [2:0] func3,  // instruction[14:12]
-  input wire bit30,  // instruction[30]
-  output reg [3:0] alu_cmd
+  input wire [2:0] aluOP,
+  input wire [2:0] func3,  
+  input wire [7:0] func7, 
+
+  output reg [2:0] alu_op,
+  output reg i_sub,       
+  output reg i_arith,   
+  output reg i_unsigned  
 );
 
-  localparam TYPE_MEM = 2'b00;
-  localparam TYPE_BR = 2'b01;
-  localparam TYPE_R = 2'b10;
-  localparam TYPE_I = 2'b11;
-
   always @(*) begin
-    case (alu_op_type)
-      TYPE_MEM:
-        alu_cmd = 4'b0000;
+    alu_op = aluOP;
+    i_sub = 1'b0;
+    i_arith = 1'b0;
+    i_unsigned = 1'b0;
 
-      TYPE_BR:
-        alu_cmd = 4'b1000;
+    case (aluOP)
+      //addition/subtraction if `i_sub` asserted
+      3'b000: i_sub = func7[5];
 
-      TYPE_R:
-        alu_cmd = {bit30, func3};
+      //set less than/unsigned if `i_unsigned` asserted
+      3'b011: i_unsigned = (func3 == 3'b011) ? 1'b1: 1'b0;
 
-      TYPE_I: begin
-        if (func3 == 3'b101 || func3 == 3'b001) begin
-          alu_cmd = {bit30, func3};
-        end else begin 
-          alu_cmd = {1'b0, func3};
-        end
-      end
+      //shift right logical/arithmetic if `i_arith` asserted
+      3'b101: i_arith = func7[5];
 
-      default: 
-        alu_cmd 4'b0000;
     endcase
   end
 
