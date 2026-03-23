944f85312bd0d5dc3a346d957184cb352e20ad91
askjsd <dhan77889@gmail.com>
2026-03-22 23:13:32 -0500

    updated branch hazard detector

--- rtl/branch_hazard_detector.v
+++ rtl/branch_hazard_detector.v
@@ -4,13 +4,9 @@ module branch_hazard_detector(
     input wire        i_rst,
     input wire [31:0] instr,
     input wire [ 5:0] imm_format,
-    input wire [ 4:0] id_ex_rd,
-    input wire        id_ex_reg_write,
-    input wire [ 4:0] ex_mem_rd,
-    input wire        ex_mem_reg_write,
-    input wire [ 4:0] mem_wb_rd,
-    input wire        mem_wb_reg_write,
-
+    input wire id_ex_mem_read,
+    input wire [ 4:0] id_ex_rd,  
+    
     output reg        if_id_write_en,
     output reg        pc_write_en,
     output reg        cu_passthrough_en
@@ -27,12 +23,8 @@ module branch_hazard_detector(
     assign rs2_rd = imm_format[0] | imm_format[2] | imm_format[3];
 
     wire hazard;
-    //if id/ex.writereg = if/id.regrs1 or rs2
-    assign hazard = ((id_ex_reg_write & (id_ex_rd != 5'b00000) & (((id_ex_rd == rs1) & rs1_rd) | ((id_ex_rd == rs2) & rs2_rd)))
-                    //if ex/mem.writereg = if/id.regrs1 or rs2
-                    | (ex_mem_reg_write & (ex_mem_rd != 5'b00000) & (((ex_mem_rd == rs1) & rs1_rd) | ((ex_mem_rd == rs2) & rs2_rd)))
-                    //if mem/wb.writereg = if/id.regrs1 or rs2
-                    | (mem_wb_reg_write & (mem_wb_rd != 5'b00000) & (((mem_wb_rd == rs1) & rs1_rd) | ((mem_wb_rd == rs2) & rs2_rd))));
+    assign hazard = id_ex_mem_read && (id_ex_rd != 5'b00000) &&
+            (((id_ex_rd == rs1) && rs1_rd)||((id_ex_rd == rs2) && rs2_rd));
 
     always @(*) begin
         case (hazard | i_rst) 
